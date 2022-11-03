bb4eb5fa0fd332c53411992be2437fb2d42315aa
Konstantyn <k.buhantsev@gmail.com>
2022-11-03 22:21:21 +0200

    fix bug

--- src/components/PhoneBook/PhoneBook.jsx
+++ src/components/PhoneBook/PhoneBook.jsx
@@ -9,17 +9,6 @@ import { ToastContainer } from 'react-toastify';
 import { useSelector } from 'react-redux';
 import { getContacts, getFilter } from '../../redux/selectors';
 
-// const INITIAL_STATE = {
-//   contacts: [
-//     { id: '928fbg1', name: 'Rosie Simpson', number: '459-12-56' },
-//     { id: '928fbg2', name: 'Hermione Kline', number: '443-89-12' },
-//     { id: '928fbg3', name: 'Eden Clements', number: '645-17-79' },
-//     { id: '928fbg4', name: 'Annie Copeland', number: '227-91-26' },
-//   ],
-// };
-
-// const STORAGE_KEY = 'contacts';
-
 export default function PhoneBook() {
   // const [contacts, setContacts] = useLocalStorage(
   //   STORAGE_KEY,
