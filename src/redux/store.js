1bc8e5d9f489759f6b1a02ca12f1cb7e0a0ac9f0
Konstantyn <k.buhantsev@gmail.com>
2022-11-09 11:46:22 +0200

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -14,9 +14,17 @@ import {
   REGISTER,
 } from 'redux-persist';
 
+const middleware = getDefaultMiddleware =>
+  getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  });
+
 const rootPersistConfig = {
   key: 'root',
   storage,
+  whitelist: ['contacts'],
 };
 
 const rootReducer = combineReducers({
@@ -28,12 +36,8 @@ const persistedReducer = persistReducer(rootPersistConfig, rootReducer);
 
 export const store = configureStore({
   reducer: persistedReducer,
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
+  middleware: middleware,
+  devTools: process.env.NODE_ENV !== 'production',
 });
 
 export const persistor = persistStore(store);
